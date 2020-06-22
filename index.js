d0716519ca6b4794b25ae2a9354cee36e06036e6
chapter414 <chapter414@gmail.com>
2020-06-22 22:44:44 +0200

    Detail Change

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const {Builder, By, Key,util} = require("selenium-webdriver");
 async function testing() {
     let driver = await new Builder().forBrowser("firefox").build();
     try {
-        await driver.get("https://www.way2automation.com/angularjs-protractor/webtables/");
+        await driver.get("http://www.way2automation.com/angularjs-protractor/webtables/");
         await driver.findElement(By.css("body > table > thead > tr:nth-child(2) > td > button")).click();
-        await driver.findElement(By.name("FirstName")).sendKeys("Anthony");
-        await driver.findElement(By.name("LastName")).sendKeys("Omotola");
-        await driver.findElement(By.name("UserName")).sendKeys("Chapter414");
-        await driver.findElement(By.name("Password")).sendKeys("Alpha@2020");
+        await driver.findElement(By.name("FirstName")).sendKeys("FName1");
+        await driver.findElement(By.name("LastName")).sendKeys("LName1");
+        await driver.findElement(By.name("UserName")).sendKeys("User1");
+        await driver.findElement(By.name("Password")).sendKeys("Pass1");
         await driver.findElement(By.name("optionsRadios")).value("15").click();
-        await driver.findElement(By.css("body > div.modal.ng-scope > div.modal-body > form > table > tbody > tr:nth-child(6) > td:nth-child(2) > select").value("Customer"));
-        await driver.findElement(By.name("Email")).sendKeys("chapter414@gmail.com");
-        await driver.findElement(By.name("Mobilephone")).sendKeys("0732465898");
+        await driver.findElement(By.css("body > div.modal.ng-scope > div.modal-body > form > table > tbody > tr:nth-child(6) > td:nth-child(2) > select").value("Admin"));
+        await driver.findElement(By.name("Email")).sendKeys("admin@mail.com");
+        await driver.findElement(By.name("Mobilephone")).sendKeys("082555");
         await driver.findElement(By.className("btn btn-success")).click();
     } catch (error) {
         console.log(error);
